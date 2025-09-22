b9e1caf75d41ccbab6332b6a9b111399b0c150d7
hassaammgl <h455a4mmehtab@gmail.com>
2025-09-22 10:49:41 +0500

    feat: enhance job listing schema with location requirements, experience levels, and job statuses; add job listing applications and organization user settings tables

--- src/drizzle/schema/jobListing.ts
+++ src/drizzle/schema/jobListing.ts
@@ -1,38 +1,75 @@
 import {
   boolean,
+  index,
   integer,
   pgEnum,
   pgTable,
   text,
+  timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
-import { id } from "../schemaHelpers";
+import { id, createdAt, updatedAt } from "../schemaHelpers";
 import { OrganizationTable } from "./organization";
 
 export const wageIntervals = ["hourly", "yearly"] as const;
-
 export type WageInterval = (typeof wageIntervals)[number];
-
 export const wageIntervalEnum = pgEnum(
   "job_listings_wage_interval",
   wageIntervals
 );
 
-export const JobListingTable = pgTable("job_listings", {
-  id,
-  organizationId: varchar()
-    .references(() => OrganizationTable.id, {
-      onDelete: "cascade",
-    })
-    .notNull(),
-  title: varchar().notNull(),
-  description: text().notNull(),
-  wage: integer(),
-  wageInterval: wageIntervalEnum(),
-  stateAbbreviation: varchar(),
-  city: varchar(),
-  isFeatured: boolean().notNull().default(false),
-});
+export const locationRequirements = ["hybrid", "remote"] as const;
+export type LocationRequirement = (typeof locationRequirements)[number];
+export const locationRequirementEnum = pgEnum(
+  "job_listings_location_requirement",
+  locationRequirements
+);
 
+export const experienceLevels = ["junior", "mid-level", "senior"] as const;
+export type ExperienceLevel = (typeof experienceLevels)[number];
+export const experienceLevelEnum = pgEnum(
+  "job_listings_experience-level",
+  experienceLevels
+);
 
-// 24 mins
+export const jobListingStatuses = ["draft", "published", "delisted"] as const;
+export type JobListingStatus = (typeof jobListingStatuses)[number];
+export const jobListingStatusEnum = pgEnum(
+  "job_listings_status",
+  jobListingStatuses
+);
+
+export const jobListingTypes = [
+  "internship",
+  "part-time",
+  "full-time",
+] as const;
+export type JobListingType = (typeof jobListingTypes)[number];
+export const jobListingTypeEnum = pgEnum("job_listings_type", jobListingTypes);
+
+export const JobListingTable = pgTable(
+  "job_listings",
+  {
+    id,
+    organizationId: varchar()
+      .references(() => OrganizationTable.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    title: varchar().notNull(),
+    description: text().notNull(),
+    wage: integer(),
+    wageInterval: wageIntervalEnum(),
+    stateAbbreviation: varchar(),
+    city: varchar(),
+    isFeatured: boolean().notNull().default(false),
+    locationRequirement: locationRequirementEnum().notNull(),
+    experienceLevel: experienceLevelEnum().notNull(),
+    status: jobListingStatusEnum().notNull().default("draft"),
+    type: jobListingTypeEnum().notNull(),
+    postedAt: timestamp({ withTimezone: true }),
+    createdAt,
+    updatedAt,
+  },
+  (table) => [index().on(table.stateAbbreviation)]
+);
---
+++ src/drizzle/schema/jobListingApplication.ts
@@ -0,0 +1,46 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  primaryKey,
+  text,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { createdAt, updatedAt } from "../schemaHelpers";
+import { JobListingTable } from "./jobListing";
+import { UserTable } from "./user";
+
+export const applicationStages = [
+  "denied",
+  "applied",
+  "interested",
+  "interviewed",
+  "hired",
+] as const;
+export type ApplicationStage = (typeof applicationStages)[number];
+export const applicationStageEnum = pgEnum(
+  "job_listing_applications_stage",
+  applicationStages
+);
+
+export const JobListingApplicationTable = pgTable(
+  "job_listing_applications",
+  {
+    jobListingId: uuid()
+      .references(() => JobListingTable.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    userId: uuid()
+      .references(() => UserTable.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    coverLetter: text(),
+    rating: integer(),
+    stage: applicationStageEnum().notNull().default("applied"),
+    createdAt,
+    updatedAt,
+  },
+  (table) => [primaryKey({ columns: [table.jobListingId, table.userId] })]
+);
