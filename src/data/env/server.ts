b32c15c22e814966c62c2b6fca5c31fe9e1b9a43
hassaammgl <h455a4mmehtab@gmail.com>
2025-09-22 14:28:38 +0500

    feat: implement Clerk layout component, update dark mode variant in CSS, and add Clerk publishable key to environment variables

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -8,15 +8,16 @@ export const env = createEnv({
     DB_HOST: z.string().min(1),
     DB_PORT: z.coerce.number().min(1).max(65535).default(5432),
     DB_NAME: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
   },
-  createFinalSchema: env => {
-    return z.object(env).transform(val => {
-        const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, ...rest } = val;
-        return {
-          ...rest,
-          DATABASE_URL: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-        };
-    })
+  createFinalSchema: (env) => {
+    return z.object(env).transform((val) => {
+      const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, ...rest } = val;
+      return {
+        ...rest,
+        DATABASE_URL: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+      };
+    });
   },
   emptyStringAsUndefined: true,
   experimental__runtimeEnv: process.env,
